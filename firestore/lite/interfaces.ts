58c0a800963f92aa0ba08d246452725751981905
developeralciautos <125672636+developeralciautos@users.noreply.github.com>
2023-10-31 08:41:12 -0500

    Update interfaces.ts
    correct possible parameter, It is considered an error, because it expects only one type parameter, it is passing three parameters

--- firestore/lite/interfaces.ts
+++ firestore/lite/interfaces.ts
@@ -7,5 +7,5 @@ export type DocumentSnapshot<T> = lite.DocumentSnapshot<T>;
 export type QuerySnapshot<T> = lite.QuerySnapshot<T>;
 export type QueryDocumentSnapshot<T> = lite.QueryDocumentSnapshot<T>;
 export type CountSnapshot = lite.AggregateQuerySnapshot<{
-  count: lite.AggregateField<number>;
-}, any, DocumentData>;
+    count: lite.AggregateField<number>;
+}>;//, any, DocumentData>;It is considered an error, because it expects only one type parameter, it is passing three parameters.
